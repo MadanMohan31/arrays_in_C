16bed0997984c86233d520f1a93b8e7f1b2cb823
Madan Mohan <126049464+MadanMohan31@users.noreply.github.com>
2023-04-25 13:59:26 +0530

    Create sum_of_elements.c

---
+++ sum_of_elements.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int test,l;
+    scanf("%d",&test);
+    for(l=1;l<=test;l++)
+    {
+        int row,i;
+        scanf("%d",&row);
+        long long a[row];
+        for(i=0;i<row;i++)
+        {
+            scanf("%lld",&a[i]);
+        }
+        long long count = 0;
+        for(i=0;i<row;i++)
+        {
+            count += a[i];
+        }
+        printf("%lld\n",count);
+    }
+
+    return 0;
+}
